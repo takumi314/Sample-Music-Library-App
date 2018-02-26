906a40179b68b5f5ed50c3149d73b4bfc1ce8a72
takumi314 <isospin_symmetry@me.com>
2018-02-26 23:28:39 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Sample-Music-Library-App' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Sample-Music-Library-App
+
+  target 'Sample-Music-Library-AppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Sample-Music-Library-AppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Sample-Music-Library-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample-Music-Library-App
+//
+//  Created by NishiokaKohei on 2018/02/26.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
