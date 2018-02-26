fd1329ec820eace790387d08b7f3e7c87981070a
takumi314 <isospin_symmetry@me.com>
2018-02-27 02:34:57 +0900

    add three methods to LibraryAPI

--- Sample-Music-Library-App/API/LibraryAPI.swift
+++ Sample-Music-Library-App/API/LibraryAPI.swift
@@ -21,4 +21,22 @@ final class LibraryAPI {
 
     }
 
+    func getAlbum() -> [Album] {
+        return persistenceManager.getAlbums()
+    }
+
+    func addAlbum(_ album: Album, at index: Int) {
+        persistenceManager.add(album, at: index)
+        if isOnline {
+            httpClient.postRequest("/api/addAlbum", body: album.description)
+        }
+    }
+
+    func deleteAlbum(at index: Int) {
+        persistenceManager.deleteAlbum(at: index)
+        if isOnline {
+            httpClient.postRequest("/api/deleteAlbum", body: "\(index)")
+        }
+    }
+
 }
