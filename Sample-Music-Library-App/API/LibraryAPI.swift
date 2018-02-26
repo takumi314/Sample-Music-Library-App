8c26d38936644eceb1894bc7f9d527c334ff9571
takumi314 <isospin_symmetry@me.com>
2018-02-27 01:19:08 +0900

    add shared static constant

---
+++ Sample-Music-Library-App/API/LibraryAPI.swift
@@ -0,0 +1,15 @@
+//
+//  LibraryAPI.swift
+//  Sample-Music-Library-App
+//
+//  Created by NishiokaKohei on 2018/02/27.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+
+final class LibraryAPI {
+
+    static let shared = LibraryAPI()
+
+}
