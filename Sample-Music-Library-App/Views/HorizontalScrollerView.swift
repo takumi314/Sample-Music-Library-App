a4f4f04d5049db2de13777002b2a51d353bf7937
takumi314 <isospin_symmetry@me.com>
2018-03-03 12:11:40 +0900

    create new class

---
+++ Sample-Music-Library-App/Views/HorizontalScrollerView.swift
@@ -0,0 +1,9 @@
+//
+//  HorizontalScrollerView.swift
+//  Sample-Music-Library-App
+//
+//  Created by NishiokaKohei on 2018/03/01.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
