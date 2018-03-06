9b1b1a29cf6a73c6798180c59260272726984597
takumi314 <isospin_symmetry@me.com>
2018-03-07 00:57:46 +0900

    implements Codable

--- Sample-Music-Library-App/Models/Album.swift
+++ Sample-Music-Library-App/Models/Album.swift
@@ -14,7 +14,7 @@ import Foundation
 ///
 typealias AlbumData = (title: String, valeu: String)
 
-struct Album {
+struct Album: Codable {
     let title : String
     let artist : String
     let genre : String
